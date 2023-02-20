a19d8c1f9b672007c821fe3a7b296427470665d4
Sven Siebler <sven.siebler@urz.uni-heidelberg.de>
2023-02-21 00:42:43 +0100

    update build script to our internal dockerswarm repo

--- build.sh
+++ build.sh
@@ -3,13 +3,17 @@ error() {
   printf '\E[31m'; echo "$@"; printf '\E[0m'
 }
 
-if [[ $EUID -ne 0 ]]; then
-    error "This script should be run using sudo or as the root user"
+# it's enough if current user is member of docker group
+if [ `groups | grep -c docker` -eq 0 -a $EUID -ne 0 ]; then
+    error "This script should be run by a user with docker access"
     exit 1
 fi
 
 export TAG=${1:-latest}
 export ARCH=${2:-"$(uname -m)"}
+
+# our local repository
+export REPO="swarm-registry:5000"
 
 build() {
     if [ $ARCH == "armv7l" ]
@@ -24,9 +28,9 @@ build() {
     fi
     if [ $ARCH == "x86_64" ]
     then
-        docker plugin rm -f mochoa/$1:$TAG || true
+        docker plugin rm -f $REPO/$1:$TAG || true
     else
-        docker plugin rm -f mochoa/$1-$ARCH:$TAG || true
+        docker plugin rm -f $REPO/$1-$ARCH:$TAG || true
     fi
     docker rmi -f rootfsimage || true
     docker buildx build --load --platform ${BPLATFORM} \
@@ -41,13 +45,15 @@ build() {
     cp $1/config.json build
     if [ $ARCH == "x86_64" ]
     then
-        docker plugin create mochoa/$1:$TAG build
-        docker plugin push mochoa/$1:$TAG
+        docker plugin create $REPO/$1:$TAG build
+        docker plugin push $REPO/$1:$TAG
+	docker plugin rm -f $REPO/$1:$TAG || true        
     else
-        docker plugin create mochoa/$1-$ARCH:$TAG build
-        docker plugin push mochoa/$1-$ARCH:$TAG
+        docker plugin create $REPO/$1-$ARCH:$TAG build
+        docker plugin push $REPO/$1-$ARCH:$TAG
+        docker plugin rm -f $REPO/$1-$ARCH:$TAG || true        
     fi
 }
 build glusterfs-volume-plugin
-build s3fs-volume-plugin
-build cifs-volume-plugin
+#build s3fs-volume-plugin
+#build cifs-volume-plugin
